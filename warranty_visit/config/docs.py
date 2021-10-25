2cf70fb2d8b407f4ec7974ce2480d1d148bcb315
Mizna <fathimamk@yahoo.com>
2021-10-25 11:14:40 +0400

    Create App

---
+++ warranty_visit/config/docs.py
@@ -0,0 +1,11 @@
+"""
+Configuration for docs
+"""
+
+# source_link = "https://github.com/[org_name]/warranty_visit"
+# docs_base_url = "https://[org_name].github.io/warranty_visit"
+# headline = "App that does everything"
+# sub_heading = "Yes, you got that right the first time, everything"
+
+def get_context(context):
+	context.brand_html = "Warranty Visit"
